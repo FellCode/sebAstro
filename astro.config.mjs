c36103a0430d7a0aee4ff8c4a327bab34431edde
fvogel <felix.vogel@adesso-insurance-solutions.de>
2026-03-16 11:23:05 +0100

    Überarbeitung
    # Conflicts:
    #	package-lock.json
    #	src/components/Footer.astro
    #	src/components/Header.astro
    #	src/pages/datenschutz.astro
    #	src/pages/impressum.astro
    #	src/pages/index.astro
    #	src/pages/infos.astro
    #	src/pages/jugend.astro
    #	src/pages/majestaeten.astro
    #	src/pages/schiesssport.astro
    #	src/pages/vorstand.astro
    #	src/pages/where.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
   site: "https://fellcode.github.io/sebAstro",
   base: "/sebAstro/",
   trailingSlash: "always"
